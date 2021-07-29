acd1ce9ac0e43e766633875606d5dcaa9790d7fa
Vansh Mohan <69002944+vanshman@users.noreply.github.com>
2021-07-29 17:04:21 +0530

    nice
    nice

--- main.js
+++ main.js
@@ -2,12 +2,28 @@ function preload(){
 
 }
 function setup(){
-    canvas = createCanvas(350, 350);
+    canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses)
+}
+function modelLoaded(){
+    console.log("posenet initialized");
 }
 function draw(){
-
+    image(video, 0, 0, 300, 300);
 }
 function take_snapshot(){
     save("filter.png");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.Y;
+    }
 }
